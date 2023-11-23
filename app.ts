726d810bce41abae3eaf753d0d49c325ebbd5f6c
ibar <ibar.student@simad.edu.so>
2023-11-23 15:14:01 +0300

    comment about cookie

--- app.ts
+++ app.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import {auth} from './api/auth';
 import mongoose from 'mongoose';
 import { createuser } from './api/createuser';
+
+//i don't know if i would need cokie when my app is just for api and not a webapp
 var cookieparser = require('cookie-parser');
 
 
